68eaeb86c2ab13c4fa3bf701e686b72d59933fb7
Alan Wynn <alan.wynn@friendlydev.com>
2021-02-14 10:30:00 +0000

    fix bash so works for me

--- R710-IPMI-TEMP/R710-IPMIStatic.sh
+++ R710-IPMI-TEMP/R710-IPMIStatic.sh
@@ -3,21 +3,20 @@
 # ----------------------------------------------------------------------------------
 # Script for setting manual fan speed to 2160 RPM (on my R710)
 #
+# https://www.spxlabs.com/blog/2019/3/16/silence-your-dell-poweredge-server
+#
 # Requires:
 # ipmitool – apt-get install ipmitool
-# slacktee.sh – https://github.com/course-hero/slacktee
 # ----------------------------------------------------------------------------------
-
 
 # IPMI SETTINGS:
 # Modify to suit your needs.
 # DEFAULT IP: 192.168.0.120
-IPMIHOST=10.0.100.20
+IPMIHOST=172.16.0.125
 IPMIUSER=root
 IPMIPW=calvin
 IPMIEK=0000000000000000000000000000000000000000
 
-printf "Activating manual fan speeds! (2160 RPM)" | systemd-cat -t R710-IPMI-TEMP
-echo "Activating manual fan speeds! (2160 RPM)" | slacktee.sh -t "R710-IPMI-TEMP [$(hostname)]"
+printf "Activating manual fan speeds! (2160 RPM)"
 ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW -y $IPMIEK raw 0x30 0x30 0x01 0x00
 ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW -y $IPMIEK raw 0x30 0x30 0x02 0xff 0x09
--- R710-IPMI-TEMP/R710-IPMITemp.sh
+++ R710-IPMI-TEMP/R710-IPMITemp.sh
@@ -1,19 +1,17 @@
-#!/bin/bash
-
 # ----------------------------------------------------------------------------------
 # Script for checking the temperature reported by the ambient temperature sensor,
 # and if deemed too high send the raw IPMI command to enable dynamic fan control.
 #
+# https://www.spxlabs.com/blog/2019/3/16/silence-your-dell-poweredge-server
+#
 # Requires:
 # ipmitool – apt-get install ipmitool
-# slacktee.sh – https://github.com/course-hero/slacktee
 # ----------------------------------------------------------------------------------
-
 
 # IPMI SETTINGS:
 # Modify to suit your needs.
 # DEFAULT IP: 192.168.0.120
-IPMIHOST=10.0.100.20
+IPMIHOST=172.16.0.125
 IPMIUSER=root
 IPMIPW=calvin
 IPMIEK=0000000000000000000000000000000000000000
@@ -21,21 +19,19 @@ IPMIEK=0000000000000000000000000000000000000000
 # TEMPERATURE
 # Change this to the temperature in celcius you are comfortable with.
 # If the temperature goes above the set degrees it will send raw IPMI command to enable dynamic fan control
-MAXTEMP=27
+MAXTEMP=65
 
 # This variable sends a IPMI command to get the temperature, and outputs it as two digits.
 # Do not edit unless you know what you do.
-TEMP=$(ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW -y $IPMIEK sdr type temperature |grep Ambient |grep degrees |grep -Po '\d{2}' | tail -1)
-
+TEMP=$(ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW -y $IPMIEK sdr type temperature | grep -o -E -i 'Temp .+ (\d{2} degrees)' | tail -1 | grep -o -E -i '(\d{2})')
 
 if [[ $TEMP > $MAXTEMP ]];
-  then
-    printf "Warning: Temperature is too high! Activating dynamic fan control! ($TEMP C)" | systemd-cat -t R710-IPMI-TEMP
-    echo "Warning: Temperature is too high! Activating dynamic fan control! ($TEMP C)" | /usr/bin/slacktee.sh -t "R710-IPMI-TEMP [$(hostname)]"
-    ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW -y $IPMIEK raw 0x30 0x30 0x01 0x01
-  else
-    # healthchecks.io
-    curl -fsS --retry 3 https://hchk.io/XXX >/dev/null 2>&1
-    printf "Temperature is OK ($TEMP C)" | systemd-cat -t R710-IPMI-TEMP
-    echo "Temperature is OK ($TEMP C)"
+    then
+        echo "Warning: Temperature is too high! Activating dynamic fan control! ($TEMP C)"
+        ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW -y $IPMIEK raw 0x30 0x30 0x01 0x01
+    else
+        echo "Temperature is OK ($TEMP C)"
+        echo "Activating manual fan speeds! (30% Fan Speed)"
+        ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW -y $IPMIEK raw 0x30 0x30 0x01 0x00
+        ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW -y $IPMIEK raw 0x30 0x30 0x02 0xff 0x1e
 fi
